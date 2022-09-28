1e591448a5ad3d0a396710f292eba0deb701fe9c
Glitch (tricky-shimmering-epoch) <none>
2022-09-28 20:47:03 +0000

    👔👮 Checkpoint ./komutlar/botlist.js:32852719/22374 ./komutlar/botlistkomut.js:32852719/21405 ./ayarlar.json:32852719/77 ./komutlar/botkist-yardım.js:32852719/618

--- komutlar/botkist-yardım.js
+++ komutlar/botkist-yardım.js
@@ -12,25 +12,25 @@ exports.run = async (client, message, args) => {
 .setColor('#660099')
 .setTitle('Mavera Botlist Menüsü')
 .setFooter("MaveraBot", client.user.avatarURL())
-.setThumbnail('https://media.discordapp.net/attachments/1022843509016895568/1023170182463627345/143d6bc7-3090-43dd-854c-def1789613a9.png?width=435&height=433')
-.setImage("https://media.discordapp.net/attachments/1022843509016895568/1023194392246374411/ezgif.com-gif-maker.gif?width=374&height=48")
+.setThumbnail('https://images-ext-1.discordapp.net/external/qejo50XEQHbwkly87vSlkZ12obuaYGLNvTzQpYAb3G4/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/1023174970249056277/3bd5b0ec6744a35c20be353ccfc2b0d1.webp?width=433&height=433')
+.setImage("https://media.discordapp.net/attachments/1022843509016895568/1024035336847896616/Mercy_Afis.png?width=576&height=324")
 .setDescription(`*Ana Komutlar;*
 
-**<:51:1010178990155255808> m!bot ekle** - Bot eklersiniz.
-**<:51:1010178990155255808> m!bot onayla** - Bot onaylarsınız.
-**<:51:1010178990155255808> m!bot reddet** - Bot reddedersiniz.
-**<:51:1010178990155255808> m!bot bilgi** - Bot hakkında bilgi alırsınız.
-**<:51:1010178990155255808> m!botlist sıfırla hepsi** - Sistemi Ful Sıfırlar.
+**<:51:1010178990155255808> !bot ekle** - Bot eklersiniz.
+**<:51:1010178990155255808> !bot onayla** - Bot onaylarsınız.
+**<:51:1010178990155255808> !bot reddet** - Bot reddedersiniz.
+**<:51:1010178990155255808> !bot bilgi** - Bot hakkında bilgi alırsınız.
+**<:51:1010178990155255808> !botlist sıfırla hepsi** - Sistemi Ful Sıfırlar.
 
 *Ayarlama Komutları;*
 
-**<:51:1010178990155255808> m!botlist ayarlar** - Botlist ayarlarını görürsünüz.
-**<:51:1010178990155255808> m!botlist kanal** - Botlist kanal ayarlarsınız.
-**<:51:1010178990155255808> m!botlist devepoler-rol** - Botlist devepoler rol ayarlarsınız.
-**<:51:1010178990155255808> m!botlist yetkili-rol** - Botlist yetkili rol ayarlarısınız.
-**<:51:1010178990155255808> m!botlist log** - Botlist log ayarlarsınız.
-**<:51:1010178990155255808> m!botlist başvuru-log** - Botlist başvuru log ayarlarsınız.
-**<:51:1010178990155255808> m!botlist dm-takip aç** - Botlist dm takip sistemini ayarlarsınız.
+**<:51:1010178990155255808> !botlist ayarlar** - Botlist ayarlarını görürsünüz.
+**<:51:1010178990155255808> !botlist kanal** - Botlist kanal ayarlarsınız.
+**<:51:1010178990155255808> !botlist devepoler-rol** - Botlist devepoler rol ayarlarsınız.
+**<:51:1010178990155255808> !botlist yetkili-rol** - Botlist yetkili rol ayarlarısınız.
+**<:51:1010178990155255808> !botlist log** - Botlist log ayarlarsınız.
+**<:51:1010178990155255808> !botlist başvuru-log** - Botlist başvuru log ayarlarsınız.
+**<:51:1010178990155255808> !botlist dm-takip aç** - Botlist dm takip sistemini ayarlarsınız.
 `)
 
  message.channel.send(embed) 
@@ -49,7 +49,7 @@ exports.conf = {
 
 exports.help = {
 
-  name: 'botlistyardım',
+  name: 'yardım',
 
   description: 'Yardım Menüsünü Açar',
 
